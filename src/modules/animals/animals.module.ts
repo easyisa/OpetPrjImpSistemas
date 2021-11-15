40acd50d541f0130f5a7e36ce297ef6512e5039d
easyIsa <isabel.yoshino@medprevonline.com>
2021-11-15 14:14:02 -0300

    feat: add table log

--- src/modules/animals/animals.module.ts
+++ src/modules/animals/animals.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Animal } from './entities'
+import { AnimalLog } from './entities/animalLog.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Animal])],
+  imports: [TypeOrmModule.forFeature([Animal, AnimalLog])],
   exports: [TypeOrmModule]
 })
 export class AnimalModule {}
